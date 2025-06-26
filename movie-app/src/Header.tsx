17eb2c4d6eb69d0a2e914d95c8489e49c942a14d
yuilhan01 <yui.kasakawa@esm.co.jp>
2025-06-26 11:52:39 +0900

    映画

--- movie-app/src/Header.tsx
+++ movie-app/src/Header.tsx
@@ -2,7 +2,7 @@ function Header({ children }: { children: React.ReactNode }) {
   return (
     <div className="app-bg">
       <header className="app-header">
-        <h1 className="app-title">YUICHANFLIX</h1>
+        <h1 className="app-title">MOVIEFLIX</h1>
       </header>
       <main>{children}</main>
     </div>
